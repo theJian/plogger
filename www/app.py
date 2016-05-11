e5932853d53fa8a425339f11df7e180719e676a0
theJian <thejianmail@gmail.com>
2016-05-12 01:22:22 +0800

    app init work

--- www/app.py
+++ www/app.py
@@ -2,13 +2,74 @@ import logging; logging.basicConfig(level=logging.INFO)
 import asyncio, os, json, time
 from datetime import datetime
 from aiohttp import web
+from jinja2 import Environment, FileSystemLoader
+import orm
+from coroweb import add_routes, add_static
+
+def init_jinja2(app, **kw):
+    logging.info('init jinja2 ...')
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+    env = Environment(
+            loader=kw.get('path', path),
+            autoescape=kw.get('autoescape', True),
+            block_start_string=kw.get('block_start_string', '{%'),
+            block_end_string=kw.get('block_end_string', '%}'),
+            variable_start_string=kw.get('variable_start_string', '{{'),
+            variable_end_string=kw.get('variable_end_string', '}}'),
+            auto_reload=kw.get('auto_reload', True)
+            )
+    filters = kw.get('filters', None)
+    if filters is not None:
+        for name, fn in filters.items():
+            env.filters[name] = fn
+    app['__templating__'] = env
+
+async def logger_factory(app, handler):
+    async def logger(request):
+        logging.info('Request: %s %s' % (request.method, request.path))
+        return await handler(request)
+    return logger
+
+async def response_factory(app, handler):
+    async def response(request):
+        logging.info('Response handler ...')
+        r = await handler(request)
+        if isinstance(r, web.StreamResponse):
+            return r
+        if isinstance(r, bytes):
+            res = web.Response(body=r)
+            res.content_type = 'application/octet-stream'
+            return res
+        if isinstance(r, str):
+            res = web.Response(body=r.encode('utf-8'))
+            res.content_type = 'text/html;charset=utf-8'
+            return res
+        if isinstance(r, dict):
+            template = r.get('__template__')
+            if template is None:
+                res = web.Response(body=json.dumps(r, ensure_ascii=False).encode('utf-8'))
+                res.content_type = 'application/json;charset=utf-8'
+                return res
+            else:
+                res = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                res.content_type = 'text/html;charset=utf-8'
+                return res
+        res = web.Response(body=str(r).encode('utf-8'))
+        res.content_type = 'text/plain;charset=utf-8'
+        return res
+    return response
 
 def index(req):
     return web.Response(body=b'<h1>Awesome</h1>')
 
 async def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
+    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='user', password='password', db='db')
+    app = web.Application(loop=loop, middlewares=[
+         logger_factory,
+         response_factory
+        ])
+    init_jinja2(app, 'handlers')
+    add_static(app)
     srv = await loop.create_server(app.make_handler(), '127.0.0.1', 2333)
     logging.info('server started at http://127.0.0.1:2333...')
     return srv
