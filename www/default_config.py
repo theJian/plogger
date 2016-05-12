ff35ede90b20ba903ec3220a7e1d2ad215f9bb9f
theJian <thejianmail@gmail.com>
2016-05-12 21:43:14 +0800

    update config file

---
+++ www/default_config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+configs = {
+    'host':'127.0.0.1',
+    'port':3306,
+    'user':'user',
+    'password':'password',
+    'db':'db',
+    'secret': 'catcatcat'
+}
