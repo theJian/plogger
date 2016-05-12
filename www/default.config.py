d71e013833e75bbe9f27f4f30b59980479fa5262
theJian <thejianmail@gmail.com>
2016-05-12 17:07:16 +0800

    add default config

---
+++ www/default.config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+configs = {
+    'database': {
+        host='127.0.0.1',
+        port=3306
+        user='user',
+        password='password',
+        db='db'
+    },
+    'session': {
+        'secret': 'catcatcat'
+    }
+}
